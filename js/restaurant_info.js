79fac37dc3e66bbaaff7bd0d43670b3de481dd71
Sophia Mai <phuongmai001@gmail.com>
2018-09-07 11:31:30 -0700

    make every link an anchor tag

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -176,8 +176,13 @@ createReviewHTML = (review) => {
 fillBreadcrumb = (restaurant=self.restaurant) => {
   const breadcrumb = document.getElementById("breadcrumb");
   const li = document.createElement("li");
-  li.innerHTML = restaurant.name;
-  li.ariaCurrent = "page";
+  // li.innerHTML = restaurant.name;
+  // li.ariaCurrent = "page";
+  const a = document.createElement("a");
+  a.href = window.location;
+  a.innerHTML = restaurant.name;
+  a.setAttribute("aria-current", "page");
+  li.appendChild(a);
   breadcrumb.appendChild(li);
 };
 
